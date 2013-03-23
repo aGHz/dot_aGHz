807034765e029efe1435c64d13071c0c58b41c18
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2013-03-23 18:22:25 -0400

    Added better git log output formats

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -7,15 +7,18 @@ export LS_OPTIONS
 alias ll="ls $LS_OPTIONS -l"
 alias lall="ls $LS_OPTIONS -l -a"
 
-alias grep="grep --color=always -n"
+alias grep="grep --color=always"
 alias G="grep -r --exclude=*\.svn* "
 alias F="find . -name "
 
 # Git aliases
-alias glog="git log --format=format:'%w(200,0,4)%Cblue%aN%Creset: %s'"
-alias glogful="git log --format=format:'%Cblue%aN%Creset: %Cgreen%H%Creset%n%B'"
+#alias glog="git log --format=format:'%w(200,0,4)%Cblue%aN%Creset: %s'"
+alias glog="git log --graph --date=short --format=format:'%w(200,0,4)%C(yellow)%ad %Cblue%aN%Creset: %Cgreen%h%Creset  %s'"
+alias glogd="git log --date=iso --format=format:'%w(200,0,4)%C(yellow)%ad %Cblue%aN%Creset: %s'"
+alias glogful='git log --date=iso --format=format:'\''%Cblue%aN%Creset: %Cgreen%H%Creset %C(yellow)%ad%Creset%n%B'\'''
 alias gst="git status"
-alias gd="git diff HEAD"
+alias gdvim="git diff HEAD"
+alias gd="git diff HEAD --no-ext-diff"
 
 # Mercurial aliases
 alias hglog='hg log --template '\''\033[32m{rev}\033[0m | \033[34m{author|user}\033[0m\t | {desc} \033[32m{date|age} on {date|rfc822date}\033[0m\n'\'''
